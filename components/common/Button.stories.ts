71b1c898aa9b49f0fdafadc81cc2a208ea54e0af
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-07-29 17:51:59 +0900

    feat: 버튼 컴포넌트 만들기 + 컬러 아이콘 추가 (#4)
    * feat: 테일윈드 변수명 수정
    
    * feat: 버튼 컴포넌트 제작 + 스토리북에 등록
    
    * feat: color icon 추가

---
+++ components/common/Button.stories.ts
@@ -0,0 +1,99 @@
+import type { Meta, StoryObj } from "@storybook/nextjs";
+import Button, { ButtonProps } from "./Button";
+
+const meta: Meta<ButtonProps> = {
+  title: "Components/Button",
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    State: {
+      control: { type: "select" },
+      options: [
+        "Default",
+        "Primary",
+        "Sub",
+        "Solid",
+        "Disabled",
+        "Secondary",
+        "Focused",
+      ],
+    },
+    Size: {
+      control: { type: "select" },
+      options: ["xs", "sm", "md", "lg", "xl", "xxl", "xxxl"],
+    },
+    onClick: { action: "clicked" },
+    label: { control: "text" },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Playground: Story = {
+  args: {
+    State: "Default",
+    Size: "md",
+    label: "Click Me",
+  },
+};
+
+export const Default: Story = {
+  args: {
+    State: "Default",
+    Size: "md",
+    label: "Default",
+  },
+};
+
+export const Primary: Story = {
+  args: {
+    State: "Primary",
+    Size: "md",
+    label: "Primary",
+  },
+};
+
+export const Sub: Story = {
+  args: {
+    State: "Sub",
+    Size: "md",
+    label: "Sub",
+  },
+};
+
+export const Solid: Story = {
+  args: {
+    State: "Solid",
+    Size: "md",
+    label: "Solid",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    State: "Disabled",
+    Size: "md",
+    label: "Disabled",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    State: "Secondary",
+    Size: "md",
+    label: "Secondary",
+  },
+};
+
+export const Focused: Story = {
+  args: {
+    State: "Focused",
+    Size: "md",
+    label: "Focused",
+  },
+};
+
